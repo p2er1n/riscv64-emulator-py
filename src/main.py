d9fdd00019292b571fdd1d437e8e11c813c7af2e
Peerin <1461200635@qq.com>
2024-08-30 17:43:38 +0800

    fix(main): new initilization for `mem` to conform new `mem` implementation

--- src/main.py
+++ src/main.py
@@ -3,15 +3,13 @@ from mem import Mem
 from utils.asm import asm
 
 if __name__ == "__main__":
-    mem = Mem(0x1000)
-    mem.write(0x0,
-              bytes(asm("addi", "x0", "x0", 1)) + 
-              bytes(asm("addi", "x1", "x1", 2)) +
-              bytes(asm("addi", "x2", "x2", 3)) +
-              bytes(asm("addi", "x0", "x4", 666))
-              )
+    mem = Mem(m_size=2**20*100)
+    mem.loadBytes(
+        bytes(asm("addi","x0","x0",1)) +
+        bytes(asm("addi","x1","x1",1)) +
+        bytes(asm("add","x0","x0","x1"))
+                  )
     hart1 = Hart(mem)
-    hart1.try_step()
     hart1.try_step()
     hart1.try_step()
     hart1.try_step()
